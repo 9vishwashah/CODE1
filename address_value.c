8fc7ce45db493580ca5fa9793f6c6d61766654ef
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-06-10 23:16:32 +0530

    Create address_value.c

---
+++ address_value.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int n = 3;
+    int *p = &n;
+
+    printf("Value of n is: %d\n", n);
+    printf("Address of n is: %x\n", &n);
+    
+    printf("Value of ptr is: %x\n", p);
+    printf("Value at ptr is: %d\n", *p);
+}
