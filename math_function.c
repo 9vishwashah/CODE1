a2ba5e2d812fcf8ea08dd52afc9d05303160f352
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-06-10 08:33:12 +0530

    Create math_function.c
    This is a program of math library functions

---
+++ math_function.c
@@ -0,0 +1,12 @@
+#include<stdio.h>  
+#include <math.h>  
+#include<stdlib.h>
+int main(){        
+printf("%f\n",ceil(3.3));        
+printf("%f\n",floor(3.2));    
+printf("%f\n",sqrt(16));      
+printf("%f\n",pow(2,4));        
+printf("%d\n",abs(-12));  
+printf("%lf\n",fmod(8.2,3));
+return 0;    
+}    
