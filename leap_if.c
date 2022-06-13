608261e5cd1e82ceecf4c45004ceff7e79a53e3d
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-06-13 19:05:03 +0530

    Create leap_if.c
    This is a program that identifies whether the year is leap year or not using If else statements.

---
+++ leap_if.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter leap year: ");
+    scanf("%d", &n);
+    if (n % 400 == 0) 
+    {
+        printf("It is a leap year\n");
+    } 
+    else if (n % 100 == 0) 
+    {
+        printf("It is not a leap year\n");
+    } 
+    else if (n % 4 == 0) 
+    {
+        printf("It is a leap year\n");
+    } 
+    else 
+    {
+        printf("It is not a leap year\n");
+    }
+}
