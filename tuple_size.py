3b1cd11f18f485d303e55dd9d14d4d79020b01d1
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-06-26 09:53:15 +0530

    Create tuple_size.py

---
+++ tuple_size.py
@@ -0,0 +1,7 @@
+import sys
+T1 = ("v", 1, "v", 2, "v", 3)
+T2 = ("VISHWA", "ALPESH", "SHAH")
+T3 = ((1, "vishwa"), ( 2, "alpesh"), (3, "shah"))
+print("Size of Tuple1: " + str(sys.getsizeof(T1)) + "bytes")
+print("Size of Tuple2: " + str(sys.getsizeof(T2)) + "bytes")
+print("Size of Tuple3: " + str(sys.getsizeof(T3)) + "bytes")
