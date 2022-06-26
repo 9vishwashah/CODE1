a150356efbe55b85d0dd31888ad784c1c4b9a9d7
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-06-26 09:15:55 +0530

    Create dict_values_ordered.py

---
+++ dict_values_ordered.py
@@ -0,0 +1,4 @@
+test_dict = {"Vishwa" : 1, "Alpesh" : 2, "Shah" : 3}
+print("The original dictionary is : " + str(test_dict))
+res = list(test_dict.keys()) + list(test_dict.values())
+print("The ordered keys and values : " + str(res))
