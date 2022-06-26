5c3f3fcc628f44ce05188dce2ba1650ace000ae4
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-06-26 07:22:53 +0530

    Create remove_key.py

---
+++ remove_key.py
@@ -0,0 +1,5 @@
+a = {"nihu" : 22, "vishu" : 21, "rashu" : 21, "sunflower" : 21}
+print ("The dictionary before performing remove is : " + str(a))
+del a['sunflower']
+print ("The dictionary after remove is : " + str(a))
+
