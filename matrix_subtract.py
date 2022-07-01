3cd5446bb88e459ab502b3cca56729c1f73ae323
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-01 07:07:09 +0530

    Create matrix_subtract.py

---
+++ matrix_subtract.py
@@ -0,0 +1,9 @@
+import numpy as np
+A = np.array([[1, 2], [3, 4]])
+B = np.array([[4, 5], [6, 7]])
+print("Printing elements of first matrix")
+print(A)
+print("Printing elements of second matrix")
+print(B)
+print("Subtraction of two matrix")
+print(np.subtract(A, B))
