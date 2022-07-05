49484681f21db66c6c2cdf5cf2c51b398c37e3c0
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-05 10:38:24 +0530

    Create replace.py

---
+++ replace.py
@@ -0,0 +1,16 @@
+name = input("Enter the name: ")
+
+date = input("Enter the date: ")
+
+template = '''
+
+Dear <|name|>,
+
+you are selected
+
+<|date|>
+
+'''
+
+print(template.replace('<|name|>', name).replace('<|date|>', date))
+
