7c66779bafb6bb5b558881be7f33942ac933a560
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-05 10:35:25 +0530

    Create string_functions.py

---
+++ string_functions.py
@@ -0,0 +1,18 @@
+myStr = "abcdefghijklmnopqrstuvwxyzaaab"
+
+print(len(myStr))
+
+print(myStr.endswith("xyz"))
+
+print(myStr.startswith("abcd"))
+
+print(myStr.count('ab'))
+
+myStr = "my name is vishwa shah"
+
+print(myStr.capitalize())
+
+print(myStr.find("name"))
+
+print(myStr.replace("name", "date"))
+
