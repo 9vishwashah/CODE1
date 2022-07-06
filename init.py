e7f8a125cbebf1b9ce2f02d6535db1762c656483
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-06 10:12:46 +0530

    Create init.py

---
+++ init.py
@@ -0,0 +1,16 @@
+class Programmer:
+
+    def __init__(self, name, language):
+
+        self.name = name
+
+        self.language = language
+
+vishwa = Programmer("vishwa", "Python")
+
+rohan = Programmer("Rohan", "Java")
+
+print(rohan.name)
+
+print(vishwa.name)
+
