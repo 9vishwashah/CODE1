46a79f6e292a04272dfa2d148e1774b9882d8bab
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-06 10:04:23 +0530

    Create class.py

---
+++ class.py
@@ -0,0 +1,16 @@
+class Employee:
+
+    name = "Vishwa"
+
+    marks = 34
+
+    center = "Delhi"
+
+harry = Employee() # A basic object
+
+print(harry.marks)
+
+print(harry.center)
+
+print(harry.name)
+
