c405cebc9726cab9a84668226485cad99db9aa5e
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-06 10:07:33 +0530

    Create class_methods.py

---
+++ class_methods.py
@@ -0,0 +1,24 @@
+class Employee:
+
+    name = "vishwa"
+
+    marks = 34
+
+    center = "Delhi"
+
+    def printObj(self):
+
+        print(f"The name is {self.name}")
+
+vishwa = Employee() 
+
+shyam = Employee() 
+
+print(vishwa.marks)
+
+print(vishwa.center)
+
+print(vishwa.name)
+
+vishwa.printObj() 
+
