f7d3b36303b29639c3fd5a8e9d2da278ede2eb54
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-13 09:28:42 +0530

    Update circle_area_vol.c

--- circle_area_vol.c
+++ circle_area_vol.c
@@ -1,20 +1,9 @@
 #include <stdio.h>
-
 int main()
-
 {
-
-    int radius = 3;
-
-    float pi = 3.14;
-
-    printf("The area of this circle is %f\n", pi * radius * radius);
-
+    int r = 3;
+    printf("The area of this circle is %f\n", 3.141592653 * radius * radius);
     int height = 3;
-
-    printf("Volume of this cylinder is %f\n", pi * radius * radius * height);
-
-    return 0;
-
+    printf("Volume of this cylinder is %f\n", 3.141592653 * radius * radius * height);
+return 0;
 }
-
