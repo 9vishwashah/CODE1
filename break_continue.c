96b9e4d61f492f8d1ccaa014c0d98abdf5787572
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-13 09:13:18 +0530

    Update break_continue.c

--- break_continue.c
+++ break_continue.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 int main()
 {
-  for (int i = 1; i <=5; i++) 
-   {
-        if (i == 3)
+    for (int i = 1; i <=5; i++) 
         {
-            break;
+            if (i == 3)
+                {
+                     break;
+                }
+            printf("%d\n", i);
         }
-        printf("%d\n", i);
-    }
-  printf("\n");
-
+    printf("\n");
     for (int i = 1; i <= 5; i++) 
-   { 
-        if (i == 3) 
-       {
-            continue;
+        { 
+            if (i == 3) 
+                {
+                    continue;
+                }
+            printf("%d\n", i);
         }
-        printf("%d\n", i);
-   }
 }
