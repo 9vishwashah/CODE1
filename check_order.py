e9e38f2717a5e66862b82e3b3ebd7bac86d7841f
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-13 09:26:31 +0530

    Update check_order.py

--- check_order.py
+++ check_order.py
@@ -6,9 +6,9 @@ def checkOrder(a, b):
 		if (key == b[n]):
 			n = n + 1
 		if (n == (len(b))):
-			return 'true'
-	return 'false'
+			return 'true the entered char is in order'
+	return 'false entered char is not in order'
 if __name__ == "__main__":
 	a = input("Enter the String\n")
-	b = input("Enter letter you want to check in order")
+	b = input("Enter letter you want to check in order\n")
 	print (checkOrder(a,b))
