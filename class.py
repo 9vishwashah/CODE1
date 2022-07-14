dc3622bf859379a36d1ac00b48755382083180ef
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-14 09:13:44 +0530

    Update class.py

--- class.py
+++ class.py
@@ -1,16 +1,8 @@
 class Employee:
-
     name = "vishwa"
-
     marks = 34
-
     center = "Delhi"
-
 vishwa= Employee() # A basic object
-
 print(vishwa.marks)
-
 print(vishwa.center)
-
 print(vishwa.name)
-
