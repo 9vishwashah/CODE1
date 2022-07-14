5e668b25a78faf293114daaf14bb305ed427ac12
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-14 09:14:44 +0530

    Update class_attribute.py

--- class_attribute.py
+++ class_attribute.py
@@ -1,40 +1,19 @@
 class Employee:
-
-    name = "vishwa" # A class attribute
-
+    name = "vishwa" 
     marks = 34
-
     center = "Delhi"
-
     def printObj(self):
-
         print(f"The name is {self.name}")
-
-    
-
     @staticmethod
-
     def greet():
-
         print("good day")
-
 Employee.name = "vishwashah" 
-
 vishwa = Employee() 
-
 shyam = Employee() 
-
 print(vishwa.name) 
-
 print(shyam.name)
-
 shyam.name = "Shyam" 
-
 print(shyam.name)
-
 print(vishwa.name)
-
 vishwa.greet()
-
 Employee.greet()
-
