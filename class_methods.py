f6b8de26f18fae22d4c85064ac95a0c8369a06d9
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-14 09:16:40 +0530

    Update class_methods.py

--- class_methods.py
+++ class_methods.py
@@ -1,24 +1,12 @@
 class Employee:
-
     name = "vishwa"
-
     marks = 34
-
     center = "Delhi"
-
     def printObj(self):
-
         print(f"The name is {self.name}")
-
 vishwa = Employee() 
-
 shyam = Employee() 
-
 print(vishwa.marks)
-
 print(vishwa.center)
-
 print(vishwa.name)
-
 vishwa.printObj() 
-
