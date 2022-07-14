5ffcb1c5d3469e32e564093b8bc8df1a0df486c2
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-14 09:16:02 +0530

    Update class_complex.py

--- class_complex.py
+++ class_complex.py
@@ -2,11 +2,8 @@ class Complex:
     def __init__(self,a, b) -> None:
         self.a = a
         self.b = b
-
     def __add__(self, obj):
         return Complex(self.a + obj.a, self.b + obj.b)
-
-
 c1 = Complex(1, 4)
 c2 = Complex(11, 3)
 c3 = c1+c2
