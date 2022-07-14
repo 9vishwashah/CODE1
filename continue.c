93fd512bf47cc48c5de78a33096cbc6fead0dc2b
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-14 09:21:41 +0530

    Update continue.c

--- continue.c
+++ continue.c
@@ -1,28 +1,19 @@
 #include<stdio.h>
-
-int main(){
-
+int main()
+{
     int skip=5, i=0;
+    while(i<10)
+        {
+            i++;
+                if(i!=skip)
+                    {
+                        continue;
+                    }
 
-    while(i<10){
-
-        i++;
-
-        if(i!=skip){
-
-            continue;
-
+                else    
+                    {
+                        printf("%d\n", i);
+                    }
         }
-
-        else{
-
-            printf("%d\n", i);
-
-        }
-
-    }
-
-    return 0;
-
+return 0;
 }
-
