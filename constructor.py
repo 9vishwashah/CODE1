626e92417dd4d768ba8c201c6e4324489804b4ea
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-14 09:19:15 +0530

    Update constructor.py

--- constructor.py
+++ constructor.py
@@ -1,36 +1,17 @@
 class Employee: 
-
     center = "Not known"
-
     def __init__(self, name, marks, center): 
-
         self.name = name 
-
         self.marks = marks 
-
         self.center = center 
-
     def printObj(self):
-
         print(f"The name is {self.name}")
-
         print(f"The marks is {self.marks}") 
-
         print(f"The center is {self.center}") 
-
-    
-
     @staticmethod
-
     def greet():
-
         print("good day")
-
 vishwa = Employee("vishwa", 34, "Delhi")
-
 rohan = Employee("Rohan", 94, "Kolkata")
-
 vishwa.printObj()
-
 rohan.printObj()
-
