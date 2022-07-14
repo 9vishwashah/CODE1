748030ef37eb82ac5eb14c99c99d34c03d130041
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-14 09:30:55 +0530

    Update do_while.c

--- do_while.c
+++ do_while.c
@@ -1,24 +1,13 @@
 #include<stdio.h>
-
-int main(){
-
+int main()
+{
     int i=0;
-
     int n;
-
     printf("Enter the value of n\n");
-
     scanf("%d", &n);
-
-    do{
-
-        printf("The number is %d \n", i+1);
-
-        i++;
-
-    }while(i<n);
-
-    return 0;
-
+        do{
+               printf("The number is %d \n", i+1);
+               i++;
+          }while(i<n);
+return 0;
 }
-
