66f9aa6b9ecccff46d3e7d38f92a9a42b1983f1f
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-15 14:02:48 +0530

    Update value.c

--- value.c
+++ value.c
@@ -1,20 +1,13 @@
 #include<stdio.h>
-
-int main(){
-
+int main()
+{
     int n;
-
     printf("Enter the value of n \n");
-
     scanf("%d", &n);
-
-    for(int i=n; i ; i--){
-
-        printf("The value of i is %d\n", i);
-
-    }
-
-    return 0;
-
+        for(int i=n; i ; i--)
+            {
+                printf("The value of i is %d\n", i);
+            }
+return 0;
 }
 
