7f9cff78a21ee3a43a96e204c63ac6d669919210
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-15 14:09:07 +0530

    Update word_remove.py

--- word_remove.py
+++ word_remove.py
@@ -1,15 +1,8 @@
 def process(l, word):
-
     word = word.strip()
-
     if word in l:
-
         l.remove(word) 
-
     return l1
-
 l1 = ['vishwa', 'rohan', 'akash', 'shubham', 'lovish']
-
 l1 = process(l1, '   rohan   ')
-
 print(l1)
