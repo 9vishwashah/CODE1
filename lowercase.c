2a380a3e43f57b3dfd73d7e3d46e15a02ec60a1b
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-15 11:13:30 +0530

    Update lowercase.c

--- lowercase.c
+++ lowercase.c
@@ -1,26 +1,17 @@
 #include<stdio.h>
-
-int main(){
-
+int main()
+{
     char ch;
-
     printf("Enter the character\n");
-
     scanf("%c", &ch);
-
-    if(ch<=122 && ch>=97){
-
-        printf("It is lowercase");
-
-    }
-
-    else{
-
-        printf("It is not lowercase");
-
-    }
-
-    return 0;
-
+        if(ch<=122 && ch>=97)
+            {
+                printf("It is lowercase");
+            }
+        else
+            {
+                printf("It is not lowercase");
+            }
+return 0;
 }
 
