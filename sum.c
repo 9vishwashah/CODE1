e4928d199f966871658d7ba304f60fd970a7b8f0
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-15 13:32:46 +0530

    Update sum.c

--- sum.c
+++ sum.c
@@ -1,20 +1,13 @@
 #include<stdio.h>
-
-int main(){
-
+int main()
+{
     int i=1, sum=0, n=10;
-
-    while( i<=n){
-
-        sum +=i;
-
-        i++; 
-
-    }
-
+        while( i<=n)
+            {
+                sum +=i;
+                i++; 
+            }
     printf("The value of sum(1 to 10) is %d", sum);
-
     return 0;
-
 }
 
