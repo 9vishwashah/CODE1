98a8d6034b62796a1a129d6c1bb99d7dcf1c2814
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-15 11:07:27 +0530

    Update list_functions.py

--- list_functions.py
+++ list_functions.py
@@ -1,18 +1,11 @@
 myListOfNumbers = [1, 8, 7, 2, 21, 15]
-
 print(myListOfNumbers)
-
 myListOfNumbers.sort()
-
 myListOfNumbers.reverse()
-
-myListOfNumbers.append(9)  myListOfNumbers.insert(2, 9) 
-
+myListOfNumbers.append(9)  
+myListOfNumbers.insert(2, 9) 
 myListOfNumbers.pop()  
-
 myListOfNumbers.pop(2)  
-
 myListOfNumbers.remove(21)   
-
 print(myListOfNumbers)
 
