9f8970cb81140c614a36aa7c2f7f95264f2b32ce
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-15 09:33:30 +0530

    Update largest_3num_if.c

--- largest_3num_if.c
+++ largest_3num_if.c
@@ -4,16 +4,16 @@ int main()
     int a, b, c;
     printf("Enter three numbers:\n");
     scanf("%d %d %d", &a, &b, &c);
-    if (a >= b && a >= c) 
-    {
-        printf("%d is largest\n", a);
-    } 
-    else if (b >= a && b >= c) 
-    {
-        printf("%d is largest\n", b);
-    } 
-    else if (c >= a && c >= b) 
-    {
-        printf("%d is largest\n", c);
-    }
+        if (a >= b && a >= c) 
+            {
+                printf("%d is largest\n", a);
+            } 
+        else if (b >= a && b >= c) 
+            {
+                printf("%d is largest\n", b);
+            } 
+        else if (c >= a && c >= b) 
+            {
+                printf("%d is largest\n", c);
+            }
 }
