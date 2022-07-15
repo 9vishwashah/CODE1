2361b36529cf7a546b1f6a0d436b76187c279234
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-15 08:33:55 +0530

    Update factorial_function.c

--- factorial_function.c
+++ factorial_function.c
@@ -8,20 +8,19 @@ int main()
     int f=fact(n);
     printf("Factorial = %d",f);
 }
-  int fact(int n)
-{
-  if (n==0)
-{
-    return 0;
-}
-  else if (n==1)
-{
-    return 1;
-}
-  else 
-{
-    return n*fact(n-1);
-}
+int fact(int n)
+    {
+        if (n==0)
+            {
+                return 0;
+             }
+        else if (n==1)
+            {
+                return 1;
+            }
+        else 
+            {
+                return n*fact(n-1);
+            }
  return 0;
 }
-  
