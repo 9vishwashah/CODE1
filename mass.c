c235a618b2ffaa9eceb87978b25c243407515cdf
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-15 11:17:38 +0530

    Update mass.c

--- mass.c
+++ mass.c
@@ -1,26 +1,16 @@
 #include<stdio.h>
-
 float force(float mass);
-
-int main(){
-
+int main()
+{
     float m;
-
     printf("Enter the value of mass in kgs\n");
-
     scanf("%f", &m);
-
     printf("The value of force in Newton is %.2f\n", force(m)); 
-
-    return 0;
-
+return 0;
 }
-
-float force(float mass){
-
-    float result = mass * 9.8;
-
-    return result;
-
-}
+float force(float mass)
+    {
+        float result = mass * 9.8;
+        return result;
+    }
 
