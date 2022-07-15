0250ee85210187d9c0f3a4a50dca429dee470fa7
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-15 08:44:43 +0530

    Update file_read.py

--- file_read.py
+++ file_read.py
@@ -1,22 +1,4 @@
 f = open("this.txt", "r")
-
-# text = f.read(6)
-
-# text = f.readline()
-
-# print(text)
-
-# text = f.readline()
-
-# print(text)
-
-# text = f.readline()
-
-# print(text)
-
 textList = f.readlines()
-
 print(textList)
-
 f.close()
-
