bc31d8bef4eee81df1deb56ae8b85378755fa103
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-15 13:38:58 +0530

    Update table_10.c

--- table_10.c
+++ table_10.c
@@ -1,15 +1,10 @@
 #include<stdio.h>
-
-int main(){
-
+int main()
+{
     printf("****Multiplication table of 10****\n\n");
-
-    for(int i=10;i;i--){
-
-        printf("10 X %d = %d\n", i, 10*i);
-
-    }
-
-    return 0;
-
+        for(int i=10;i;i--)
+            {
+                printf("10 X %d = %d\n", i, 10*i);
+            }
+return 0;
 }
