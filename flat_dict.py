5eac6b33b79bef8bdcb5307666e931bf2e90c368
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-15 08:49:41 +0530

    Update flat_dict.py

--- flat_dict.py
+++ flat_dict.py
@@ -1,6 +1,6 @@
 from itertools import product
 a = {'num' : [1, 2, 3],
-			'month' : ['Jan', 'Feb', 'March']}
+     'month' : ['Jan', 'Feb', 'March']}
 print("The original dictionary is : " + str(a))
 res = dict(zip(a['num'], a['month']))
 print("Flattened dictionary : " + str(res))
