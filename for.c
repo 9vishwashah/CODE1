9dc2b302c5ee60774d768b66bddefeb300cf0b3f
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-15 08:52:17 +0530

    Update for.c

--- for.c
+++ for.c
@@ -1,14 +1,9 @@
 #include<stdio.h>
-
-int main(){
-
-    for(int a=0; a<10; a++){
-
-        printf("The value of a is %d \n", a+1);
-
-    }
-
-    return 0;
-
+int main()
+{
+    for(int a=0; a<10; a++)
+        {
+            printf("The value of a is %d \n", a+1);
+        }
+return 0;
 }
-
